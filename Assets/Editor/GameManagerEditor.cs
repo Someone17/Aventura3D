9eca4edcf2a1b4c80172ed1fa25de9f9676d529a
Someone17 <40706918+Someone17@users.noreply.github.com>
2024-12-24 01:05:15 -0300

    Add files via upload

--- Assets/Editor/GameManagerEditor.cs
+++ Assets/Editor/GameManagerEditor.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(GameManager))]
+[CustomEditor(typeof(FSM))]
 public class GameManagerEditor : Editor
 {
      public override void OnInspectorGUI()
